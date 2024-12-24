40290b27604b3c9b28a75ab0e2934c55b75d4c92
jihun4452 <jihun6548@naver.com>
2024-12-24 15:20:43 +0900

    feat: 문자열 갯수 구하기

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static int solutoin(String str, char t){
+        int answer = 0;
+        str = str.toUpperCase();
+        t=Character.toUpperCase(t);
+        System.out.println(str);
+        for(char x : str.toCharArray()){
+            if(x==t){
+                answer++;
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String str=sc.nextLine();
+        char c=sc.next().charAt(0);
+        System.out.println(solutoin(str,c));
+
+    }
+}
