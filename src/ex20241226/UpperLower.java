016b025e1eb312f377077dff56821101b9f98b46
박지훈 <jihun6548@naver.com>
2024-12-28 14:11:09 +0900

    feat: 특정 문자열 뒤집기

--- src/ex20241226/UpperLower.java
+++ src/ex20241226/UpperLower.java
@@ -3,25 +3,54 @@ package ex20241226;
 import java.util.Scanner;
 
 public class UpperLower {
+    public static int solution(String str, char c){
+        int answer=0;
 
-    public static String solution(String str){
-        String answer="";
+        str=str.toUpperCase();
+        c=Character.toUpperCase(c);
 
         for(char x:str.toCharArray()){
-            if(x == Character.toLowerCase(x)){
-                answer+=Character.toUpperCase(x);
-            }else {
-                answer+=Character.toLowerCase(x);
+            if(x==c){
+                answer++;
             }
         }
-        //상향된 for 문에 대해 더 알아보자
+        System.out.println(str);
 
         return answer;
     }
-
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String str = sc.nextLine();
-        System.out.println(solution(str));
+        Scanner sc=new Scanner(System.in);
+        String str=sc.nextLine();
+        char c= sc.next().charAt(0);
+        System.out.println(solution(str,c));
     }
 }
+
+
+//package ex20241226;
+//
+//import java.util.Scanner;
+//
+//public class UpperLower {
+//
+//    public static String solution(String str){
+//        String answer="";
+//
+//        for(char x:str.toCharArray()){
+//            if(x == Character.toLowerCase(x)){
+//                answer+=Character.toUpperCase(x);
+//            }else {
+//                answer+=Character.toLowerCase(x);
+//            }
+//        }
+//        //상향된 for 문에 대해 더 알아보자
+//
+//        return answer;
+//    }
+//
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        String str = sc.nextLine();
+//        System.out.println(solution(str));
+//    }
+//}
