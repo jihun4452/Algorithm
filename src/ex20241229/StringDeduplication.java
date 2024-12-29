28b0c2f2627c0eb9f75ffce729334dcb3f464d1c
박지훈 <jihun6548@naver.com>
2024-12-29 21:19:10 +0900

    feat: 중복 문자열 제거

---
+++ src/ex20241229/StringDeduplication.java
@@ -0,0 +1,21 @@
+package ex20241229;
+
+import java.util.Scanner;
+
+public class StringDeduplication {
+    public static String solution(String str) {
+        String answer="";
+
+        for(int i=0;i<str.length();i++) {
+            if(str.indexOf(str.charAt(i))==i) answer+=str.charAt(i);
+        }//인덱스 오브로 처음 발견된 문자열이 발견, charAt과 같은지 비교 같다면 그것만 Answer에 추가한다.
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        String str= sc.nextLine();
+        System.out.println(solution(str));
+    }
+}
