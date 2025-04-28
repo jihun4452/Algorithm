2592e70a3515963462333570168b631d122141a8
박지훈 <jihun6548@naver.com>
2025-04-28 11:49:05 +0900

    feat: 회문 문자열 문제 다시 복습

--- src/ex25/ex250426/Main.java
+++ src/ex25/ex250426/Main.java
@@ -4,21 +4,25 @@ import java.util.Scanner;
 
 public class Main {
     public static String soultion(String str) {
-        String answer = "";
-        int len=str.length();
+        String answer = "YES";
 
-        for(int i=0;i<len;i++) {
-            if(i== str.indexOf(str.charAt(i))){
-                answer+=str.charAt(i);
+        str = str.toLowerCase();
+
+        int len = str.length();
+        for(int i = 0; i < len/2; i++) {
+            if(str.charAt(i) != str.charAt(len-1-i)) {
+                answer = "NO";
+                break;
             }
         }
 
         return answer;
     }
 
+
     public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         String str= sc.nextLine();
-        System.out.println(soultion(str));
+        System.out.print(soultion(str));
     }
 }
